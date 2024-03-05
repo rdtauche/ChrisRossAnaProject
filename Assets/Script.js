2e9cf0b1f4deac3974951bdec31c830b6c3f2357
CJ <csh.joo77@gmail.com>
2024-03-04 20:39:09 -0500

    added working youtube link with the API and properly displaying YT video for our *promotional* added JS code

--- Assets/Script.js
+++ Assets/Script.js
@@ -4,7 +4,7 @@ var price14k = document.querySelector(".price14k");
 var price18k = document.querySelector(".price18k");
 var price22k = document.querySelector(".price22k");
 var price24k = document.querySelector(".price24k");
-
+var tag = document.createElement('script');
 
 var myHeaders = new Headers();
 myHeaders.append("x-access-token", "goldapi-90ztrlt55ljoy-io");
@@ -31,3 +31,43 @@ function displayData(metal) {
     price24k.innerText=`${metal.price_gram_24k}`
 
   }
+
+
+
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '1200',
+          width: '650',
+          videoId: 'xuuNZQwhEn4&ab',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+      var player;
